ef708c3d338fb3071bfbc813ba593b6c3fe15298
dubandro <dubovik@internet.ru>
2022-12-19 14:35:30 +0200

    init

---
+++ src/test/java/ru/geekbrains/dubandro/marketapp/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.dubandro.marketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
